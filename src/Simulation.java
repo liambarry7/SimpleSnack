cb6f95bcc47571ddd603ee332775ad1dce46c1f4
LB7 <liambarry406@yahoo.co.uk>
2024-04-28 23:31:50 +0100

    begun silumation class, need to fix accountID to be string in customer classes

--- src/Simulation.java
+++ src/Simulation.java
@@ -1,6 +1,7 @@
 import Objects.*;
 
 import java.io.File;
+import java.util.Scanner;
 
 public class Simulation {
     public static void main(String[] args) {
@@ -37,7 +38,9 @@ public class Simulation {
 //            System.out.println("Error: " + e);
 //        }
 
-//        initialiseShop("UEA Shop", snacks.txt, customer.txt);
+        File snackFile = new File("snacks.txt");
+        File customerFile = new File("customers.txt");
+        initialiseShop("UEA Shop", snackFile, customerFile);
 
 
     }
@@ -45,12 +48,74 @@ public class Simulation {
     public static SnackShop initialiseShop(String shopName, File snackFile, File customerFile) {
         SnackShop newSnackShop = new SnackShop(shopName);
 
+        try {
+            Scanner scanSnack = new Scanner(snackFile);
+            while (scanSnack.hasNextLine()) {
+                String line = scanSnack.nextLine();
+                String[] snackValues = line.split("@");
+                System.out.println(line);
+
+                for (int i = 0; i < snackValues.length; i++) {
+                    System.out.println(snackValues[i]);
+
+                    if (snackValues[0].contains("D")) { // if drink
+                        Drink newDrink = new Drink(snackValues[0], snackValues[1], snackValues[2], Integer.parseInt(snackValues[3]));
+                        newSnackShop.addSnack(newDrink);
+
+                    } else if (snackValues[0].contains("F")) {
+                        Food newFood = new Food(snackValues[0], snackValues[1], Boolean.parseBoolean(snackValues[2]), Integer.parseInt(snackValues[3]));
+                        newSnackShop.addSnack(newFood);
+
+                    }
+                }
+
+                System.out.println();
+
+
+
+            }
+
+            System.out.println(newSnackShop.getSnack("D/7154984"));
+
+
+        } catch(Exception e) {
+            System.out.println("Error: " + e);
+        }
+
+        try {
+            Scanner scanCust = new Scanner(customerFile);
+            while (scanCust.hasNextLine()) {
+                String line = scanCust.nextLine();
+                String[] custValues = line.split("#");
+                System.out.println(line);
+
+                for (int i = 0; i < custValues.length; i++) {
+                    System.out.println(custValues[i]);
+
+                    if (custValues.length == 2) {
+                        Customer newCustomer = new Customer(Integer.parseInt(custValues[0]), custValues[1]);
+                        System.out.println(newCustomer);
+
+                    } else if (custValues.length == 3) {
+                        Customer newCustomer = new Customer(Integer.parseInt(custValues[0]), custValues[1], Integer.parseInt(custValues[2]));
+                        System.out.println(newCustomer);
+
+                    }
+                }
+            }
+
+
+            System.out.println("ae");
+        } catch(Exception e) {
+            System.out.println("Error: " + e);
+        }
+
+
         // read snackFile
         // create snack object from each line
 
         // read customer file
         // create customer object from each line
-
 
 
 
